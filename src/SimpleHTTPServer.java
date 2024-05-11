aafcee5d3e305c1111f2fd3945ab5b7d6654349e
Keeran Bezuidenhout <keeranbezuidenhout@gmail.com>
2024-05-11 21:20:10 +0200

    Simple HTTP Server receiving a request and responding with the current date.

---
+++ src/SimpleHTTPServer.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class SimpleHTTPServer {
+    public static void main(String[] args) throws IOException {
+        final ServerSocket server = new ServerSocket(8080);
+        System.out.println("Listening for connection on port 8080 ....");
+
+        while (true) {
+            try (Socket clientSocket = server.accept()) {
+                Date today = new Date();
+                String response = "HTTP/1.1 200 OK\r\n\r\n" + today;
+                clientSocket.getOutputStream().write(response.getBytes("UTF-8"));
+            }
+        }
+    }
+}
